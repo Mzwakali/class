9178cd9f8710992c588dce592a0c2f55f95204ae
Mzwakali <33451503+Mzwakali@users.noreply.github.com>
2019-02-14 01:55:56 +0200

    Update App.java

--- src/main/java/za/ac/cput/projects/App.java
+++ src/main/java/za/ac/cput/projects/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Mr. Kewuti!" );
+		System.out.println(" Cloned and Modified your code, Lol");
     }
 }
